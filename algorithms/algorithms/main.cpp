494f05750539abbf79d4ba05db93678bda388386
Rafael Alba <rafael.alba.lopez@gmail.com>
2013-11-08 19:03:22 -0800

    implemented insertion sort

--- algorithms/algorithms/main.cpp
+++ algorithms/algorithms/main.cpp
@@ -8,11 +8,42 @@
 
 #include <iostream>
 
+using namespace std;
+
+// debugging functions
+void print_array(int arr[], int length)
+{
+    for (int i = 0; i < length; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+
+void insertion_sort(int unsorted[], int length)
+{
+    for (int i = 1; i < length; i++) {
+        int current = unsorted[i];
+        
+        int k = i - 1;
+        
+        while (k >= 0) {
+            int comparing = unsorted[k];
+            
+            if (current >= comparing) {
+                break;
+            } else {
+                unsorted[k+1] = comparing;
+                unsorted[k] = current;
+                k--;
+            }
+        }
+    }
+}
+
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int unsorted[10] = {9, 8 , 7 , 6, 5, 4, 3, 2, 1, 0};
+    insertion_sort(unsorted, 10);
     return 0;
 }
 
