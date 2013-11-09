2535cd9bd7d75cd74231027c54004955c89e98eb
Rafael Alba <rafael.alba.lopez@gmail.com>
2013-11-08 18:32:43 -0800

    Initial Commit

---
+++ algorithms/algorithms/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  algorithms
+//
+//  Created by Rafael Alba on 11/8/13.
+//  Copyright (c) 2013 Rafael Alba. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
