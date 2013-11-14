1915b085c812a6c8b08b95fe4b80b2457b366a0c
Rafael Alba <rafael.alba.lopez@gmail.com>
2013-11-13 22:52:31 -0800

    implemented janky merge sort

--- algorithms/algorithms/main.cpp
+++ algorithms/algorithms/main.cpp
@@ -18,6 +18,13 @@ void print_array(int arr[], int length)
     cout << endl;
 }
 
+void print_array_pointer(int * arr, int length)
+{
+    for (int i = 0; i < length; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
 
 void insertion_sort(int unsorted[], int length)
 {
@@ -58,12 +65,91 @@ void selection_sort(int unsorted[], int length)
     
 }
 
+int * merge_sort(int unsorted[], int length)
+{
+    if (length == 1) {
+        int * result = new int[1];
+        result[0] = unsorted[0];
+        return result;
+    }
+    if (length == 2) {
+        int * result = new int[2];
+        if (unsorted[0] > unsorted[1]) {
+            result[0] = unsorted[1];
+            result[1] = unsorted[0];
+        } else {
+            result[0] = unsorted[0];
+            result[1] = unsorted[1];
+        }
+        return result;
+    }
+    
+    int dividingIndex;
+    if (length % 2 == 1) {
+        dividingIndex = (length / 2);
+    } else {
+        dividingIndex = (length / 2) - 1;
+    }
+    
+    int leftLength = dividingIndex + 1;
+    int rightLength = length - dividingIndex - 1;
+    int * unsorted_left_side = new int[leftLength];
+    int * unsorted_right_side = new int[rightLength];
+    for (int i = 0; i <= dividingIndex; i++) {
+        unsorted_left_side[i] = unsorted[i];
+    }
+    
+    for (int i = leftLength; i <= length; i++) {
+        unsorted_right_side[i - dividingIndex - 1] = unsorted[i];
+    }
+    
+    int * sorted_left = merge_sort(unsorted_left_side, leftLength);
+    int * sorted_right = merge_sort(unsorted_right_side, rightLength);
+    delete [] unsorted_left_side;
+    delete [] unsorted_right_side;
+
+    int * sorted = new int[length];
+
+    int k = 0;
+    int l = 0;
+    for (int i = 0; i < length; i++) {
+        if (k < leftLength && l < rightLength) {
+            int left = sorted_left[k];
+            int right = sorted_right[l];
+            if (left < right) {
+                sorted[i] = left;
+                k++;
+            } else {
+                sorted[i] = right;
+                l++;
+            }
+
+        } else {
+            if (k < leftLength) {
+                sorted[i] = sorted_left[k];
+                k++;
+            } else {
+                sorted[i] = sorted_right[l];
+                l++;
+            }
+        }
+    }
+    
+    delete [] sorted_left;
+    delete [] sorted_right;
+    
+    return sorted;
+}
+
 int main(int argc, const char * argv[])
 {
     int unsorted[10] = {9, 8 , 7 , 6, 5, 4, 3, 2, 1, 0};
+    print_array(unsorted, 10);
 //    insertion_sort(unsorted, 10);
 //    selection_sort(unsorted, 10);
-    print_array(unsorted, 10);
+    int * sorted = merge_sort(unsorted, 10);
+    print_array(sorted, 10);
+    delete [] sorted;
     return 0;
 }
 
